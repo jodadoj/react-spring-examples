1d9b69b2fb7641540c32c661a2b5f429e51310c3
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-02-07 13:39:07 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import "./App.css";
+import { Mesh } from "three";
+
+function MyRotatingBox() {
+  const myMesh = useRef<THREE.Mesh>(new Mesh);
+  const [active, setActive] = useState(false);
+
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime();
+    myMesh.current.rotation.x = a;
+  });
+
+  return (
+    <mesh
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      ref={myMesh}
+    >
+      <boxBufferGeometry />
+      <meshPhongMaterial color="royalblue" />
+    </mesh>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <Canvas>
+        <MyRotatingBox />
+        <ambientLight intensity={0.1} />
+        <directionalLight />
+      </Canvas>
+    </div>
+  );
+}
