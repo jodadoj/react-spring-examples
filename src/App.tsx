d7c3a817e70b6601fbb0c62f32c3e81b66e335d5
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-02-08 02:47:32 +0000

    A few options

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import { useSpring, animated, config } from '@react-spring/three'
 function MyRotatingBox() {
   const myMesh = useRef<THREE.Mesh>(new Mesh);
   const [active, setActive] = useState(false);
+  const [hovered, setHover] = useState(false)
+  // const [cubePosition, setCubePosition] = useState<[x:number,y:number,z:number]>([0,0,0])
+
+  // setCubePosition([Math.random()*3,Math.random()*3,Math.random()*3])
 
   useFrame(({ clock }) => {
     const a = clock.getElapsedTime();
@@ -15,18 +19,27 @@ function MyRotatingBox() {
   });
 
   const springs = useSpring({ 
-    scale: active ? 3 : 1,
-    config: config.gentle,  
+    scale: hovered ? 3 : 1,
+    postion: active ? [0,0,-3] : [0,0,0],
+    rotation: hovered ? Math.PI*4 : Math.PI*0.5,
+    // color: active ? 'hotpink' : 'orange',
+    config: config.wobbly,  
    })
+
 
   return (
     <animated.mesh 
       scale={springs.scale} 
+      position={springs.postion}
+      rotation-z={springs.rotation}
       onClick={() => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
       ref={myMesh}
     >
       <boxGeometry />
       <meshPhongMaterial color="royalblue" />
+      {/* <meshPhongMaterial color={springs.color} /> */}
     </animated.mesh>
   );
 }
